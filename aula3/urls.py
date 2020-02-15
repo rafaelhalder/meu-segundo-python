a4654c4005fb41d3685883e05c90f6a83161ef44
rafaelhalder <ra-falog@hotmail.com>
2020-02-15 16:03:56 -0300

    [aula3] GET e POST

--- aula3/urls.py
+++ aula3/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, setacookie, redireciona
+from .views import index, setacookie, redireciona, show_code, show_get_values, show_post_values
 
 app_name = 'aula3'
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', index),
     path('cookie',setacookie),
     path('uol',redireciona),
+    path('<int:code>', show_code),
+    path('get', show_get_values),
+    path('post', show_post_values),
 ]
--- aula3/views.py
+++ aula3/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     html = 'Bem vindo'
@@ -14,3 +15,35 @@ def setacookie(request):
 
 def redireciona(request):
     return HttpResponseRedirect('https://google.com.br')
+
+def show_code(request,code):
+    return HttpResponseRedirect(f'https://http.cat/{code}')
+
+def show_get_values(request):
+    nome = request.GET.get('nome',None)
+    if nome is None:
+        html = '<h1>Bem Vindo anonimo</h1>'
+    else:
+        html = f'<h1>Bem vindo{nome}</h1>'
+    return HttpResponse(html)
+
+@csrf_exempt
+def show_post_values(request):
+    head = ""
+    if(request.method == "POST"):
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        head = f'<h1>Bem vindo {fname} {lname}</h1>'
+
+    #import ipdb; ipdb.set_trace()
+    html = """
+        <form method=POST>
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" value="John"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        <input type="submit" value="Submit">
+        </form> 
+    """
+    html_to_response = head + html
+    return HttpResponse(html_to_response)
